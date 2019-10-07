942a1f77f7639efcaa239dca9296fdff4cd03682
Leo <leo.guignard@gmail.com>
2019-10-07 18:35:58 -0400

    minor change

--- spatial-registration.py
+++ spatial-registration.py
@@ -86,7 +86,7 @@ class trsf_parameters(object):
         self.time_tag = 'TM'
         self.bdv_unit = 'microns'
         self.bdv_voxel_size = None
-        self.do_bdv = 1
+        self.do_bdv = 0
 
         self.__dict__.update(param_dict)
         self.ref_voxel = tuple(self.ref_voxel)
