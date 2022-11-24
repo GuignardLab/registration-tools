4aa4c5b96b08465d066a75c39e68c03668922692
Léo Guignard <leo.guignard@univ-amu.fr>
2022-11-24 17:41:55 +0100

    bump version 0.1.0 -> 0.1.1

--- src/registrationtools/__init__.py
+++ src/registrationtools/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 from .time_registration import TimeRegistration, time_registration
 from .spatial_registration import SpatialRegistration, spatial_registration
