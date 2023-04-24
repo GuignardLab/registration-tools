603cf76f207b384c3037ff98f492b3ba6e0a4fb5
aliceeeeeeeeee <alice.gros@univ-amu.fr>
2023-04-24 15:00:41 +0200

    test

--- src/registrationtools/__init__.py
+++ src/registrationtools/__init__.py
@@ -2,7 +2,7 @@ __version__ = "0.1.0"
 
 from .time_registration import TimeRegistration
 from .spatial_registration import SpatialRegistration
-
+from .utils import *
 try:
     import importlib.resources as importlib_resources
 
@@ -10,7 +10,7 @@ try:
 except Exception as e:
     from importlib_resources._legacy import path
 
-    pkg = path("regsitrationtools", "data").args[0]
+    pkg = path("registrationtools", "data").args[0]
 
 image_path = pkg / "images"
 json_path_spatial = pkg / "JSON_spatial"
