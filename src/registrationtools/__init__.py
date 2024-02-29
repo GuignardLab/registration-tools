86353b4a84a9bb951e805d9321d1e390d55b0bab
Léo Guignard <leo.guignard@univ-amu.fr>
2024-02-29 09:50:43 +0100

    remove test files for 3.8>=

--- src/registrationtools/__init__.py
+++ src/registrationtools/__init__.py
@@ -23,14 +23,9 @@ if 8 < sys.version_info.minor:
     import importlib.resources as importlib_resources
 
     pkg = importlib_resources.files("registrationtools") / "data"
-else:
-    from importlib_resources._legacy import path
-
-    pkg = path("registrationtools", "data").args[0]
-
-image_path = pkg / "images"
-json_path_spatial = pkg / "JSON_spatial"
-json_path_time = pkg / "JSON_time"
+    image_path = pkg / "images"
+    json_path_spatial = pkg / "JSON_spatial"
+    json_path_time = pkg / "JSON_time"
 
 __all__ = [
     "get_paths",
